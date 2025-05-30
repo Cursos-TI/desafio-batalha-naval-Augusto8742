a07bd110eb2eb6535b5c4f42452e5836128bb27a
Augusto8742 <augustodacruzoliveira@gmail.com>
2025-05-30 21:55:52 +0000

    batalhaNaval

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,44 +1,84 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() {
-    // Cabeçalho de letras
-    char letras[10] = {'A','B','C','D','E','F','G','H','I','J'};
+#define LINHA 3
+#define COLUNA 5
 
-    // Tabuleiro 10x10 inicializado com 0 (água)
-    int tabuleiro[10][10];
-    for (int i = 0; i < 10; i++)
-        for (int j = 0; j < 10; j++)
-            tabuleiro[i][j] = 0;
-
-    // Navios nas diagonais (formando um "X")
-    for (int i = 0; i < 10; i++) {
-        tabuleiro[i][i] = 3;         // Diagonal principal ↘
-        tabuleiro[i][9 - i] = 3;     // Diagonal secundária ↙
-    }
-
-    // Navio horizontal central (linha 5, colunas E e F → [4][4] e [4][5])
-    tabuleiro[4][4] = 3;
-    tabuleiro[4][5] = 3;
-
-    // Navio vertical central (coluna D, linhas 4 e 7 → [3][3] e [6][3])
-    tabuleiro[3][3] = 3;
-    tabuleiro[6][3] = 3;
-
-    // Exibição do tabuleiro
-    printf("TABULEIRO BATALHA NAVAL\n");
-    printf("    ");
-    for (int i = 0; i < 10; i++) {
-        printf("%2c ", letras[i]);
-    }
-    printf("\n");
-
-    for (int i = 0; i < 10; i++) {
-        printf("%3d ", i + 1);
-        for (int j = 0; j < 10; j++) {
-            printf("%2d ", tabuleiro[i][j]);
+// Imprime matriz de habilidade com 0 e 3
+void exibirMatrizHabilidade(int hab[LINHA][COLUNA], const char* nome) {
+    printf("\nSaída de habilidade em %s:\n", nome);
+    for (int i = 0; i < LINHA; i++) {
+        for (int j = 0; j < COLUNA; j++) {
+            printf("%d ", hab[i][j] == 1 ? 3 : 0);
         }
         printf("\n");
     }
+}
+
+// Gera matriz Cone (triângulo para baixo)
+void gerarHabilidadeCone(int hab[LINHA][COLUNA]) {
+    for (int i = 0; i < LINHA; i++) {
+        for (int j = 0; j < COLUNA; j++) {
+            // Baseado no exemplo: 
+            // Linha 0: 0 0 1 0 0
+            // Linha 1: 0 1 1 1 0
+            // Linha 2: 1 1 1 1 1
+            if ((i == 0 && j == 2) ||
+                (i == 1 && (j >= 1 && j <= 3)) ||
+                (i == 2))
+                hab[i][j] = 1;
+            else
+                hab[i][j] = 0;
+        }
+    }
+}
+
+// Gera matriz Cruz (linha e coluna centrais)
+void gerarHabilidadeCruz(int hab[LINHA][COLUNA]) {
+    int centroLinha = LINHA / 2;  // 1
+    int centroColuna = COLUNA / 2; // 2
+    for (int i = 0; i < LINHA; i++) {
+        for (int j = 0; j < COLUNA; j++) {
+            // Exemplo cruz:
+            // 0 0 1 0 0
+            // 1 1 1 1 1
+            // 0 0 1 0 0
+            if (i == centroLinha || j == centroColuna)
+                hab[i][j] = 1;
+            else
+                hab[i][j] = 0;
+        }
+    }
+}
+
+// Gera matriz Octaedro (losango)
+void gerarHabilidadeOctaedro(int hab[LINHA][COLUNA]) {
+    int centroLinha = LINHA / 2;   // 1
+    int centroColuna = COLUNA / 2; // 2
+    for (int i = 0; i < LINHA; i++) {
+        for (int j = 0; j < COLUNA; j++) {
+            // Exemplo octaedro:
+            // 0 0 1 0 0
+            // 0 1 1 1 0
+            // 0 0 1 0 0
+            int dist = abs(i - centroLinha) + abs(j - centroColuna);
+            hab[i][j] = (dist <= 1) ? 1 : 0;
+        }
+    }
+}
+
+int main() {
+    int cone[LINHA][COLUNA];
+    int cruz[LINHA][COLUNA];
+    int octaedro[LINHA][COLUNA];
+
+    gerarHabilidadeCone(cone);
+    gerarHabilidadeCruz(cruz);
+    gerarHabilidadeOctaedro(octaedro);
+
+    exibirMatrizHabilidade(cone, "cone");
+    exibirMatrizHabilidade(cruz, "cruz");
+    exibirMatrizHabilidade(octaedro, "octaedro");
 
     return 0;
 }
